6b93d39f9683db1b4f4a56e980aa85b455807f85
wong kim hong <wongkimhong@wongs-MacBook-Air.local>
2022-11-26 18:43:33 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const menuData = [
+		{ label: "Home", link: "/" },
+		{ label: "Product", link: "/" },
+		{ label: "About", link: "/" },
+		{ label: "Contact", link: "/" },
+	];
+
+	return (
+		<div className="h-screen bg-slate-200 p-10">
+			<div className="m-4 p-2 bg-white flex justify-between items-center">
+				<h1 className="text-[14px] mx-3 text-slate-700 font-bold px-4">
+					PROSEIDON
+				</h1>
+
+				<nav className="pr-10 flex items-center">
+					<ul className="flex gap-10">
+						{menuData?.map((x) => {
+							return (
+								<li className="text-[14px] py-4 font-bold" key={x.label}>
+									{x.label}
+								</li>
+							);
+						})}
+					</ul>
+
+					<div className="mx-6">
+						<button className="text-[12px] border border-slate-600 px-8 py-1.5 leading-none rounded-full">
+							Login
+						</button>
+					</div>
+				</nav>
+			</div>
+		</div>
+	);
 }
 
 export default App;
